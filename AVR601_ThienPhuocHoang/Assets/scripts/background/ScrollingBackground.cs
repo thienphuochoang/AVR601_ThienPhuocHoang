e0c31cd601d5a06d23af576b69135681296a9e10
ThienPhuocHoang <phuoc@lol.land>
2026-03-25 22:17:04 -0700

    Update scrolling background feature

---
+++ AVR601_ThienPhuocHoang/Assets/scripts/background/ScrollingBackground.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public float scrollSpeed = 2f;
+    private float spriteHeight;
+    private Transform[] tiles;
+
+    void Start()
+    {
+        spriteHeight = GetComponent<SpriteRenderer>().bounds.size.y;
+        
+        tiles = new Transform[2];
+        tiles[0] = this.transform;
+
+        GameObject copy = Instantiate(gameObject, 
+            transform.position + Vector3.up * spriteHeight, 
+            Quaternion.identity, transform.parent);
+        
+        // Remove the script from the copy to avoid recursion
+        Destroy(copy.GetComponent<ScrollingBackground>());
+        tiles[1] = copy.transform;
+    }
+
+    void Update()
+    {
+        foreach (Transform t in tiles)
+        {
+            // Move both tiles down
+            t.Translate(Vector2.down * scrollSpeed * Time.deltaTime);
+
+            // If a tile has scrolled fully off the bottom, jump it to the top
+            if (t.position.y < -spriteHeight)
+            {
+                float highestY = Mathf.Max(tiles[0].position.y, tiles[1].position.y);
+                t.position = new Vector3(t.position.x, highestY + spriteHeight, t.position.z);
+            }
+        }
+    }
+}
